9b2bab6d848142b1d6e5366fddbae2a8c528b760
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2021-11-15 08:48:41 -0300

    feat: Page: Preview of the post

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { GetStaticProps } from "next"
+import { GetStaticPaths, GetStaticProps } from "next"
 import { useSession } from "next-auth/client";
 import { useRouter } from "next/router";
 import Head from "next/head";
@@ -56,7 +56,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
   )
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = () => {
   return {
     paths: [],
     fallback: 'blocking'
@@ -84,6 +84,7 @@ export const getStaticProps: GetStaticProps = async ({  params }) => {
   return {
     props: {
       post
-    }
+    },
+    revalidate: 60 * 30, // 30 minutes
   }
 }
